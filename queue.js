17b5a4720446693ec264946d619994aaeb189be2
clarspau <125636195+clarspau@users.noreply.github.com>
2024-01-18 22:16:06 -0800

    created dequeue method

--- queue.js
+++ queue.js
@@ -37,7 +37,17 @@ class Queue {
    * and return its value. Should throw an error if the queue is empty. */
 
   dequeue() {
+    if (!this.first) throw new Error("Queue is empty");
 
+    let temp = this.first;
+
+    if (this.first = this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+
+    return temp.val;
   }
 
   /** peek(): return the value of the first node in the queue. */
